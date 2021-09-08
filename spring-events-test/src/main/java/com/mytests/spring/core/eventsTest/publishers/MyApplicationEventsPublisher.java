6e28c435cb6f49d5258b99cbb83045734d4e0bfe
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-09-09 00:01:39 +0300

    added applicationEventMulticaster test

--- spring-events-test/src/main/java/com/mytests/spring/core/eventsTest/publishers/MyApplicationEventsPublisher.java
+++ spring-events-test/src/main/java/com/mytests/spring/core/eventsTest/publishers/MyApplicationEventsPublisher.java
@@ -4,6 +4,7 @@ import com.mytests.spring.core.eventsTest.events.AnotherApplicationEvent;
 import com.mytests.spring.core.eventsTest.events.MyApplicationEvent1;
 import com.mytests.spring.core.eventsTest.events.MyApplicationEvent2;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,16 +18,22 @@ import org.springframework.stereotype.Component;
 public class MyApplicationEventsPublisher {
 
     private final ApplicationEventPublisher publisher;
-
-    public MyApplicationEventsPublisher(ApplicationEventPublisher publisher) {
+    
+    private final ApplicationEventMulticaster applicationEventMulticaster;
+    public MyApplicationEventsPublisher(ApplicationEventPublisher publisher,
+                                        ApplicationEventMulticaster applicationEventMulticaster) {
         this.publisher = publisher;
+        this.applicationEventMulticaster = applicationEventMulticaster;
     }
 
     @Scheduled(fixedRate = 1000000)
     public void publishEvents(){
 
         publisher.publishEvent(new MyApplicationEvent1(this,"hello!"));
+        applicationEventMulticaster.multicastEvent(new MyApplicationEvent1(this,"multicast hello!"));
         publisher.publishEvent(new MyApplicationEvent2(this,"bye!"));
+        applicationEventMulticaster.multicastEvent(new MyApplicationEvent2(this,"multicast bye!"));
         publisher.publishEvent(new AnotherApplicationEvent(this));
+        applicationEventMulticaster.multicastEvent(new AnotherApplicationEvent(this));
     }
 }
